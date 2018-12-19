956461e64797dd3e83954fc086e7a162d552d890
User <user@Users-MacBook-Pro-3.local>
2018-12-19 13:38:36 -0800

    top spots project

---
+++ README.md
@@ -0,0 +1,2 @@
+Projects appends data in the data.json file
+prints it to the page with a map link for directions.
--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-$(document).ready(function() {
-// write your code here
 
+
+$(document).ready(function() {
+  
+    $.getJSON("data.json", function(data) {
+        console.log(data);
+
+    var mapLink = 'http://www.google.com/maps?q=';
+
+    data.forEach(function(output) {
+    $('#tables').append('<tr><td>' + output.name + '</td><td>' + output.description + '</td><td><a href="' + mapLink + output.location + '"><button type="button" class="btn btn-info"> open </button></a></td></tr>');
+    
+})
+    });
 });
